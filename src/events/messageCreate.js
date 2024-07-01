ff7703fea6507649f564568b34a2c70cf3bbdb50
Sloth <117414402+ImmutableVariable@users.noreply.github.com>
2024-07-01 14:35:16 -0500

    added a paste command functionality along with some refactoring

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,25 +1,5 @@
 const logger = require('../utils/logger.js');
-
-let lastPasteTime = Date.now();
-
-async function paste(message) {
-    // paste.com only allows one request every second. This isn't an ideal solution, but I don't really care.
-    if (Date.now() - lastPasteTime < 1000) {
-        await new Promise(r => setTimeout(r, 1000));
-    }
-
-    let response = await fetch("https://dpaste.com/api/", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-            "User-Agent": "Whaledev/1.0.0 (+https://github.com/ImmutableVariable/whaledev)"
-        },
-        body: "content=" + encodeURIComponent(message),
-    });
-    lastPasteTime = Date.now();
-
-    return response.headers.get("location");
-}
+const paste = require('../utils/paste.js');
 
 async function processMessage(message) {
     let amalgamation = "";
