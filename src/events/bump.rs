f9c163cef68c6816c8ee489ee593f6a204081748
ImmutableVariable <117414402+ImmutableVariable@users.noreply.github.com>
2024-07-25 21:36:41 -0500

    shifted around some stuff

---
+++ src/events/bump.rs
@@ -0,0 +1,39 @@
+use serenity::all::{ChannelId, Context, Message};
+
+
+/// This will remind you if the bump has already been detected
+pub async fn bump_message_handler(ctx: Context, msg: Message) -> Result<(), Box<dyn std::error::Error>> {
+    if let Some(title) = &msg.embeds[0].title {
+        if title != "DISBOARD: The Public Server List" {
+            return Ok(());
+        }
+    }
+
+    if let Some(description) = &msg.embeds[0].description {
+        if !description.contains("Bump done") {
+            return Ok(());
+        }
+    }
+
+    msg.channel_id
+        .say(&ctx.http, "Bump done, I will remind you in 2hr!")
+        .await?;
+
+    let reminder_channel_id = std::env::var("REMINDER_CHANNEL_ID")?.parse::<u64>()?;
+
+    // wait 2 hours before sending the reminder
+    tokio::time::sleep(tokio::time::Duration::from_secs(7200)).await;
+
+    let reminder_channel = ChannelId::new(reminder_channel_id);
+    reminder_channel
+        .say(
+            &ctx.http,
+            format!(
+                "<@&{}>, it's time to bump the server!",
+                std::env::var("REMINDER_ROLE_ID").unwrap()
+            ),
+        )
+        .await?;
+
+    Ok(())
+}
