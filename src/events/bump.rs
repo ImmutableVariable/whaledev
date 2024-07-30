456af6daeaea773f8baf347dd1ab612a4dd2b58c
ImmutableVariable <117414402+ImmutableVariable@users.noreply.github.com>
2024-07-30 14:18:53 -0500

    hopefully fixed it

--- src/events/bump.rs
+++ src/events/bump.rs
@@ -19,21 +19,26 @@ pub async fn bump_message_handler(ctx: Context, msg: Message) -> Result<(), Box<
         .say(&ctx.http, "Bump done, I will remind you in 2hr!")
         .await?;
 
-    let reminder_channel_id = std::env::var("REMINDER_CHANNEL_ID")?.parse::<u64>()?;
-
-    // wait 2 hours before sending the reminder
-    tokio::time::sleep(tokio::time::Duration::from_secs(7200)).await;
-
-    let reminder_channel = ChannelId::new(reminder_channel_id);
-    reminder_channel
-        .say(
-            &ctx.http,
-            format!(
-                "<@&{}>, it's time to bump the server!",
-                std::env::var("REMINDER_ROLE_ID").unwrap()
-            ),
-        )
-        .await?;
+    tokio::spawn(async move {
+        // wait 2 hours before sending the reminder
+        tokio::time::sleep(tokio::time::Duration::from_secs(7200)).await;
+        let reminder_channel_id = std::env::var("REMINDER_CHANNEL_ID")
+            .expect("REMINDER_CHANNEL_ID must be set")
+            .parse::<u64>()
+            .expect("REMINDER_CHANNEL_ID must be a valid u64");
+        
+        let reminder_channel = ChannelId::new(reminder_channel_id);
+        reminder_channel
+            .say(
+                &ctx.http,
+                format!(
+                    "<@&{}>, it's time to bump the server!",
+                    std::env::var("REMINDER_ROLE_ID").unwrap()
+                ),
+            )
+            .await
+            .expect("Error sending reminder message");
+    });
 
     Ok(())
 }
